0e95df6141f2f1042cdf3c4c4e443a8799901e57
Vili Taka-Anttila <vili.takaanttila@gmail.com>
2023-02-14 19:19:15 +0200

    Update main.go

--- csvreader/main.go
+++ csvreader/main.go
@@ -1,19 +1,65 @@
 package main
 
 import (
+	"bufio"
+	"encoding/csv"
 	"flag"
+	"fmt"
+	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
-	fileName := flag.String("file", "source.csv", "a string");
-	flag.Parse();
+	fileName := flag.String("file", "source.csv", "a string")
+	flag.Parse()
 
 	file, err := os.Open(*fileName)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	defer file.Close();
+	defer file.Close()
+
+	csvReader := csv.NewReader(file)
+	stdinReader := bufio.NewReader(os.Stdin)
+
+	var answersCorrect int = 0
+	var answersWrong int = 0
+
+	for {
+		record, err := csvReader.Read()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var question string = record[0]
+		var answer string = record[1]
+
+		fmt.Print(question, " ?")
+		input, err := stdinReader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		input = strings.ReplaceAll(input, "\n", "")
+
+		fmt.Println((input))
+		fmt.Println((answer))
+		if input == answer {
+			answersCorrect++
+		} else {
+			answersWrong++
+		}
+
+	}
+
+	fmt.Println("game is now over... your stats:")
+	fmt.Println(answersCorrect, answersWrong)
 }
